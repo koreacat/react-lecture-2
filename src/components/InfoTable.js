df1fe63a71ffac55e86ad54dca2a2ba44a20e12e
koreacat <kj9966@naver.com>
2024-02-26 23:01:12 +0900

    fix: 들여쓰기 수정

--- src/components/InfoTable.js
+++ src/components/InfoTable.js
@@ -2,42 +2,42 @@ import React from 'react'
 import styled from 'styled-components'
 
 const InfoTable = () => {
-    return (
-        <InfoTableWrapper>
-            <Table><tbody>
-                <TR>
-                    <TH></TH>
-                    <RioTH>리우 올림픽</RioTH>
-                    <LondonTH>런던 올림픽</LondonTH>
-                </TR>
-                <TR>
-                    <TH>기간</TH>
-                    <RioTD>8월 5일 ~ 8월 21일</RioTD>
-                    <LondonTD>7월 27일 ~ 8월 12일</LondonTD>
-                </TR>
-                <TR>
-                    <TH>개최지</TH>
-                    <RioTD>브라질, 리우</RioTD>
-                    <LondonTD>영국, 런던</LondonTD>
-                </TR>
-                <TR>
-                    <TH>참가 선수</TH>
-                    <RioTD>11,238</RioTD>
-                    <LondonTD>10,568</LondonTD>
-                </TR>
-                <TR>
-                    <TH>참가국</TH>
-                    <RioTD>207</RioTD>
-                    <LondonTD>204</LondonTD>
-                </TR>
-                <TR>
-                    <TH>종목 수</TH>
-                    <RioTD>306</RioTD>
-                    <LondonTD>302</LondonTD>
-                </TR>
-            </tbody></Table>
-        </InfoTableWrapper>
-    )
+  return (
+    <InfoTableWrapper>
+      <Table><tbody>
+        <TR>
+          <TH></TH>
+          <RioTH>리우 올림픽</RioTH>
+          <LondonTH>런던 올림픽</LondonTH>
+        </TR>
+        <TR>
+          <TH>기간</TH>
+          <RioTD>8월 5일 ~ 8월 21일</RioTD>
+          <LondonTD>7월 27일 ~ 8월 12일</LondonTD>
+        </TR>
+        <TR>
+          <TH>개최지</TH>
+          <RioTD>브라질, 리우</RioTD>
+          <LondonTD>영국, 런던</LondonTD>
+        </TR>
+        <TR>
+          <TH>참가 선수</TH>
+          <RioTD>11,238</RioTD>
+          <LondonTD>10,568</LondonTD>
+        </TR>
+        <TR>
+          <TH>참가국</TH>
+          <RioTD>207</RioTD>
+          <LondonTD>204</LondonTD>
+        </TR>
+        <TR>
+          <TH>종목 수</TH>
+          <RioTD>306</RioTD>
+          <LondonTD>302</LondonTD>
+        </TR>
+      </tbody></Table>
+    </InfoTableWrapper>
+  )
 }
 
 const InfoTableWrapper = styled.div`
